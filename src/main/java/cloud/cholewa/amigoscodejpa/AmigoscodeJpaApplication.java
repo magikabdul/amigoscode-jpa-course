5bba3ab18bf931da627e57133038bafb976666fe
Krzysztof Cholewa <krzysztof.cholewa@gmail.com>
2021-01-30 21:04:11 +0100

    added fake data using Faker

--- src/main/java/cloud/cholewa/amigoscodejpa/AmigoscodeJpaApplication.java
+++ src/main/java/cloud/cholewa/amigoscodejpa/AmigoscodeJpaApplication.java
@@ -1,8 +1,16 @@
 package cloud.cholewa.amigoscodejpa;
 
+import cloud.cholewa.amigoscodejpa.student.entity.Student;
+import cloud.cholewa.amigoscodejpa.student.repository.StudentRepository;
+import com.github.javafaker.Faker;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.event.EventListener;
+
+import java.util.Locale;
 
 @SpringBootApplication
 public class AmigoscodeJpaApplication {
@@ -11,4 +19,23 @@ public class AmigoscodeJpaApplication {
         SpringApplication.run(AmigoscodeJpaApplication.class, args);
     }
 
+//    @Bean
+//    CommandLineRunner commandLineRunner(StudentRepository studentRepository) {
+//        return args -> {
+//        Faker faker = new Faker(new Locale("pl"));
+//
+//            for (int i = 0; i < 100; i++) {
+//                String firstName = faker.name().firstName();
+//                String lastName = faker.name().lastName();
+//
+//                Student student = new Student();
+//                student.setFirstName(firstName);
+//                student.setLastName(lastName);
+//                student.setEmail(firstName + "." + lastName + "@example.pl");
+//                student.setAge(faker.number().numberBetween(20, 60));
+//
+//                studentRepository.save(student);
+//            }
+//        };
+//    }
 }
