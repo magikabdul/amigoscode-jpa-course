2fcc42e00cdf4a9cc20f7e5cf204fe3ce5c4755e
Krzysztof Cholewa <krzysztof.cholewa@gmail.com>
2021-01-30 14:03:04 +0100

    init commit

---
+++ src/main/java/cloud/cholewa/amigoscodejpa/AmigoscodeJpaApplication.java
@@ -0,0 +1,13 @@
+package cloud.cholewa.amigoscodejpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmigoscodeJpaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AmigoscodeJpaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cloud/cholewa/amigoscodejpa/AmigoscodeJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package cloud.cholewa.amigoscodejpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmigoscodeJpaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
